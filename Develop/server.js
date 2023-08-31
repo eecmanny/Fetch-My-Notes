67857488fcb61a820316ecc0b5765a55bedb105f
eecmanny <130661353+eecmanny@users.noreply.github.com>
2023-08-31 09:01:25 -0400

    Create server.js

---
+++ Develop/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
